f0a62877863031c53c8f73b95aa62a125ea2c636
jerrysaffold <jerrysaffold@knights.ucf.edu>
2021-06-06 17:56:53 -0400

    Initial commit

---
+++ src/main/java/ex02/base/App.java
@@ -0,0 +1,21 @@
+package ex02.base;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Jerry Saffold
+ */
+
+/*Exercise 2 - Counting the Number of Characters
+Create a program that prompts for an input string and displays output that shows the input string and the number of characters the string contains.
+
+Example Output
+What is the input string? Homer
+Homer has 5 characters.
+Constraints
+Be sure the output contains the original string.
+Use a single output statement to construct the output.
+Use a built-in function of the programming language to determine the length of the string.*/
+public class App {
+    public static void main(String[] args)
+        
+}
