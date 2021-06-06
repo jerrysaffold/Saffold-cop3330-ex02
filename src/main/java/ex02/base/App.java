6f66c128ebb3bc20cd3e9bf515e0587234006cd0
jerrysaffold <jerrysaffold@knights.ucf.edu>
2021-06-06 18:54:08 -0400

    Final commit

--- src/main/java/ex02/base/App.java
+++ src/main/java/ex02/base/App.java
@@ -5,8 +5,12 @@ package ex02.base;
  *  Copyright 2021 Jerry Saffold
  */
 
+
+
 /*Exercise 2 - Counting the Number of Characters
-Create a program that prompts for an input string and displays output that shows the input string and the number of characters the string contains.
+Create a program that prompts for an input string and
+displays output that shows the input string and the number of
+characters the string contains.
 
 Example Output
 What is the input string? Homer
@@ -15,7 +19,18 @@ Constraints
 Be sure the output contains the original string.
 Use a single output statement to construct the output.
 Use a built-in function of the programming language to determine the length of the string.*/
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args)
-        
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the input string? ");
+        //initialize string variable, then wait for user to enter a line of text.
+        String inputString = input.nextLine();
+        //store length of string in int count.
+        int count = inputString.length();
+
+        String outputString = inputString + " has " + count + " characters.";
+        System.out.println(outputString);
+    }
 }
